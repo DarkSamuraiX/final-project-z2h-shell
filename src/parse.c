0f37cd0b9827ae1c3dfc0f5cfe3c3c7a32be7bea
darksamuraix <darksamrai080@gmail.com>
2026-02-01 05:55:55 +0200

    fixed

--- src/parse.c
+++ src/parse.c
@@ -12,6 +12,8 @@
 
 
 void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
+    if (NULL == dbhdr) return STATUS_ERROR;
+    if (NULL == employees) return STATUS_ERROR;
     for (int i = 0; i < dbhdr->count; i++) {
         printf("Employee %d\n", i+1);
         printf("\tName: %s\n", employees[i].name);
